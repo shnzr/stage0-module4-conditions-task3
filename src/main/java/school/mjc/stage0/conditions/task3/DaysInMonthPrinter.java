3b02b4b873476641773f496932e236cb27830f92
Olim <shnzrvolim@gmail.com>
2022-09-28 03:11:30 +0500

    solution

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        if (month >= 1 && month <= 12) {
+            if (month == 2) {
+                System.out.println(28);
+            } else if (month == 4 || month == 9 || month == 6 || month == 11) {
+                System.out.println(30);
+            } else {
+                System.out.println(31);
+            }
+        }
+        else {
+            System.out.println("wrong number!");
+        }
     }
 }
